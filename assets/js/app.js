0383f269933e60bfc36262a75513c5c965399dbc
Selva Kumar <selva.proid@gmail.com>
2022-07-16 01:33:09 +0530

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,39 @@
+const btnArr = document.querySelectorAll("nav li a");
+const sectionArr = document.querySelectorAll("section");
+const ranProject = document.querySelector("#hero .btn-project");
+
+// Section Switching
+
+btnArr.forEach((btnElem,index) =>{
+    btnElem.addEventListener("click", () =>{
+        sectionArr.forEach((secElem,btnIndex) =>{
+            secElem.classList.remove("section-active");
+            btnArr[btnIndex].classList.remove("btn-active");
+        });
+        btnElem.classList.add("btn-active");
+        sectionArr[index].classList.add("section-active");
+    });
+});
+
+// Random Project
+
+const urlArr = [
+    "https://selva-sk10.github.io/fashionate/",
+    "https://selva-sk10.github.io/barberShop/"
+];
+let proButtonFun = () =>{
+    let ranVal = Math.floor(Math.random() * urlArr.length);
+    return(
+        `<a href="${urlArr[ranVal]}" target="_blank">Check out My Works</a>`
+    );
+}
+
+ranProject.innerHTML = proButtonFun();
+
+// Page Reload
+
+const proButton = document.querySelector("#hero .btn-project a");
+
+proButton.onclick = () =>{
+    setTimeout(()=>{window.location.reload(true);},1);
+}
